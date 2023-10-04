37e99c70f90a7dd5a66f28a1d510c251fa58801e
rohan <rohankolhatkar02@gmail.com>
2023-10-04 23:54:40 +0530

    commit#3 , built app UI + cards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,10 +2,83 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  // Function to navigate to a new page
+  void _navigateToPage(BuildContext context, String pageName) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => PageToNavigateTo(pageName: pageName),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Welcome to the Home Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () => _navigateToPage(context, 'Page 1'),
+              child: Card(
+                elevation: 4.0,
+                child: Column(
+                  children: [
+                    Image.asset(
+                        'assets/priscilla-du-preez-XkKCui44iM0-unsplash.jpg'), // Replace with your image
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Card 1'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () => _navigateToPage(context, 'Page 2'),
+              child: Card(
+                elevation: 4.0,
+                child: Column(
+                  children: [
+                    Image.asset(
+                        'assets/wonderlane-6zlgM-GUd6I-unsplash.jpg'), // Replace with your image
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Card 2'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+class PageToNavigateTo extends StatelessWidget {
+  final String pageName;
+
+  const PageToNavigateTo({Key? key, required this.pageName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(pageName),
+      ),
+      body: Center(
+        child: Text('This is $pageName'),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
 }
